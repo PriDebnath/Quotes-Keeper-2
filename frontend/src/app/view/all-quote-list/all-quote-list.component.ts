4d6d037cbb05c0c0cb0d15da098e25aef76b216e
Pritam <debnathpritam0802@gmail.com>
2024-05-04 14:04:58 +0530

    ui changes

--- frontend/src/app/view/all-quote-list/all-quote-list.component.ts
+++ frontend/src/app/view/all-quote-list/all-quote-list.component.ts
@@ -3,6 +3,11 @@ import { QuoteService } from 'src/app/core/services/quote/quote.service';
 import { Title } from '@angular/platform-browser';
 import {LocalStorageService} from "src/app/view/auth/services/localStorage/local-storage.service"
 import { JwtHelperService } from '@auth0/angular-jwt';
+import { of, interval, throwError, combineLatest,forkJoin} from 'rxjs';
+import { map, filter, take, catchError, mergeMap, retry, finalize } from 'rxjs/operators';
+
+
+
 
 @Component({
   selector: 'app-all-quote-list',
@@ -30,6 +35,21 @@ export class AllQuoteListComponent implements OnInit {
     this.title.setTitle(this.appTitle);
     //
     this.getAllQuoteList();
+    
+    
+const source1$ = of(1, 2, 3, 6);
+const observable2$ = of('World');
+
+console.log({source1$})
+console.log({observable2$})
+forkJoin({
+  message1: source1$,
+  message2: observable2$
+}).subscribe({
+  next: result => console.log(result), // Output: { message1: 'Hello', message2: 'World' }
+  complete: () => console.log('All observables completed')
+});
+
   }
 
   getAllQuoteList() {
