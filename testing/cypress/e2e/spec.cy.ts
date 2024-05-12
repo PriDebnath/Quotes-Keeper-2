2a72683524ded7ae3302174622bec0b73c3a70be
Pritam <debnathpritam0802@gmail.com>
2024-05-12 20:03:19 +0530

    added a custom cy command

--- testing/cypress/e2e/spec.cy.ts
+++ testing/cypress/e2e/spec.cy.ts
@@ -1,5 +1,9 @@
 describe('template spec', () => {
   it('passes', () => {
     cy.visit('https://example.cypress.io')
+    cy.performTaskWithinElement({
+      parentSelector:"body",
+      selector: "button",elementType:"button"
+    })
   })
 })
