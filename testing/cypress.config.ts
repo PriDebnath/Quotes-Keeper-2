5b86d7487b8b61e69fc4972b3a179c100436eecb
Pritam Debnath <103424346+PriDebnath@users.noreply.github.com>
2024-05-15 18:50:06 +0530

    Update cypress.config.ts

--- testing/cypress.config.ts
+++ testing/cypress.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
       // implement node event listeners here
     },
 //    baseUrl: "http://localhost:4200",
-    baseUrl: "https://quotes-keeper-2.onrender.com/",
+    baseUrl: "https://quote-keeper-2.netlify.app",
     watchForFileChanges: false,
     experimentalRunAllSpecs: true,
     chromeWebSecurity: false,
