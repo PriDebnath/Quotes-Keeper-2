9b47014eb333f95695d8499e8e09b123095f911d
Pritam <debnathpritam0802@gmail.com>
2024-07-23 12:12:54 +0530

    npx cap init - to create capacitor.config.ts

---
+++ frontend/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'quotes-keeper-2-android',
+  webDir: 'dist'
+};
+
+export default config;
