862fd02d817da33b6cdaa8a27d2eedd1f0cbab64
Pritam <debnathpritam0802@gmail.com>
2024-07-27 22:04:52 +0530

    created new app

--- frontend/capacitor.config.ts
+++ frontend/capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.example.app',
-  appName: 'quotes-keeper-2-android',
+  appName: 'quotes-keeper-2-android-by-pri',
   webDir: 'dist/quotes_keeper_2', //     "outputPath": "dist/quotes_keeper_2",
 };
 
