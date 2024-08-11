3cac595d0610b4808e90444493ffe180e41bb169
Pritam <debnathpritam0802@gmail.com>
2024-08-11 12:48:17 +0530

    Co-authored-by: Anshul Akotiya <Anshulgoplus@users.noreply.github.com>

--- backend/quotes_keeper_2/apps/accounts/serializers.py
+++ backend/quotes_keeper_2/apps/accounts/serializers.py
@@ -1,14 +1,15 @@
-from rest_framework import serializers  
-#from django.contrib.auth.models import User 
+from rest_framework import serializers
+
+# from django.contrib.auth.models import User
 from quotes_keeper_2.apps.accounts.models import CustomUser
+
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
-        fields = ['id', 'username', 'first_name', 'last_name', 'email', 'password']
-        extra_kwargs = {'password': {'write_only': True}}
+        fields = ["id", "username", "first_name", "last_name", "email", "password"]
+        extra_kwargs = {"password": {"write_only": True}}
 
     def create(self, validated_data):
         user = CustomUser.objects.create_user(**validated_data)
         return user
-
