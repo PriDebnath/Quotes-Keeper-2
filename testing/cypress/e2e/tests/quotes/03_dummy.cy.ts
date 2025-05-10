8b52a647b04f46b9f5f6c097a683800d4a503d6e
Pritam <debnathpritam0802@gmail.com>
2025-05-10 10:50:03 +0530

    generate peport for last and all-file-separately

--- testing/cypress/e2e/tests/quotes/03_dummy.cy.ts
+++ testing/cypress/e2e/tests/quotes/03_dummy.cy.ts
@@ -11,9 +11,9 @@ describe("Dummy", () => {
       email: `pri@cypress-${milliseconds}.io`,
     };
     const num1 = 2
-    const num2 = 2
+    const num2 = 3
     const sum = num1 + num2
-    const expectedSum = 4
+    const expectedSum = 5
     expect(sum).to.equal(expectedSum)
   })
 })
